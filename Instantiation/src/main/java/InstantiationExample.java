1352c3b3eda6d3257e2fb00ce184964db3b7fa84
Grammeri <reacttsxgrammer@gmail.com>
2022-12-26 03:47:21 +0000

    Saved progress on Instantiation 2022-12-26 03:47:21 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Scanner sc = new Scanner();
+        return sc;
     }
 }
