08ea208333ea63edbdddc08daead12921b8358d6
Grammeri <reacttsxgrammer@gmail.com>
2023-01-06 04:21:27 +0000

    Saved progress on GetMax 2023-01-06 04:21:27 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,11 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxNum = arr[0];
+        for (int j : arr){
+            if(j>maxNum)
+            maxNum = j;
+        }
+        return maxNum;
     }
 }
