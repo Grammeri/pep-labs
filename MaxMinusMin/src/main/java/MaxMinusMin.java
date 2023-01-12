8d97804c96995536321fb7408f10531531dbaf21
Grammeri <reacttsxgrammer@gmail.com>
2023-01-12 17:46:59 +0000

    Saved progress on MaxMinusMin 2023-01-12 17:46:59 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -1,7 +1,10 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class MaxMinusMin {
+    
+
     /**
      * Return the difference between the highest and lowest values of the List.
      * This will require you to know some of the List interface methods.
@@ -10,8 +13,20 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-      return 0;
+    
+      if (nums == null || nums.size()==0){
+        return Integer.MAX_VALUE;
+      }
+     
+if  (nums == null || nums.size() == 0) {
+  return Integer.MIN_VALUE;
+}
+return Collections.max(nums) - Collections.min(nums);
+
+
+} 
+
    }
       
-    }
+    
 
