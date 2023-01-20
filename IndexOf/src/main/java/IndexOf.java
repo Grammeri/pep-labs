cf436f1fea3c009231505c0bd03a529e8d9b2ab7
Grammeri <reacttsxgrammer@gmail.com>
2023-01-20 03:43:15 +0000

    Saved progress on IndexOf 2023-01-20 03:43:15 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -11,10 +11,15 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        if(arr==null || n<0){
-            return -1;
+           
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] == n){
+                return i;
+            }
         }
-        return arr[n];
+        return -1;
+        
+        
 
         
     }
