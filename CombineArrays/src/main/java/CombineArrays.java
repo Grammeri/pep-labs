2a1aa435299c3ebc6eeed8cf57d302ecb7b7223f
Grammeri <reacttsxgrammer@gmail.com>
2023-01-25 04:45:32 +0000

    Saved progress on CombineArrays 2023-01-25 04:45:32 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class CombineArrays {
     /**
@@ -5,12 +6,23 @@ public class CombineArrays {
      *
      * @param arr1 an array of ints.
      * @param arr2 an array of ints.
-     * @return an array containing the contents of arr1 followed by the contents of arr2.
+     * @return an array containing the contents of arr1 followed by the contents of
+     *         arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
+    public int[] combine(int[] arr1, int[] arr2) {
         int a1 = arr1.length;
         int a2 = arr2.length;
-        int combinedArray = a1 + 
-        return null;
+        int combinedArraySize = a1 + a2;
+        int[] combinedArray = new int[combinedArraySize];
+
+        for (int i = 0; i < a1; i++) {
+            combinedArray[i] = arr1[i];
+        }
+        for (int i = 0; i < a2; i++) {
+            combinedArray[a1 + i] = arr2[i];
+        }
+
+        return combinedArray;
     }
+
 }
