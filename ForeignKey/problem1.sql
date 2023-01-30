1adbbcb490f45ceca2a60e8f11517a476cc5cb33
Grammeri <reacttsxgrammer@gmail.com>
2023-01-30 18:44:09 +0000

    Saved progress on ForeignKey 2023-01-30 18:44:09 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES post(id)); 
+CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES site_user(id)); 
