b024313a222bed406dc33792089ffe85a4f3f0c2
Grammeri <reacttsxgrammer@gmail.com>
2023-02-03 06:58:43 +0000

    Saved progress on Isogram 2023-02-03 06:58:43 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,18 +1,34 @@
+import java.util.Arrays;
 
 public class Isogram {
     /**
-     * An Isogram is a word where no letters are repeated. You may assume that there will only be lowercase
+     * An Isogram is a word where no letters are repeated. You may assume that there
+     * will only be lowercase
      * letters in the String.
      *
-     * For instance, "cat", "isogram", "uncopyrightable" are isograms. "egg", "java", "programming" are not.
-     * You could use a Set of characters or a Map of characters to int or boolean to solve this problem.
-     * You could even solve it with an array of 26 booleans, and convert characters to an index of that array.
-     * It's also possible to solve this with nested for loops.... but it's not as easy or efficient.
+     * For instance, "cat", "isogram", "uncopyrightable" are isograms. "egg",
+     * "java", "programming" are not.
+     * You could use a Set of characters or a Map of characters to int or boolean to
+     * solve this problem.
+     * You could even solve it with an array of 26 booleans, and convert characters
+     * to an index of that array.
+     * It's also possible to solve this with nested for loops.... but it's not as
+     * easy or efficient.
      *
      * @param str a String.
      * @return true if str is an isogram, false otherwise.
      */
-    public boolean isIsogram(String str){
-        return false;
+    public boolean isIsogram(String str) {
+        
+        int len = str.length();
+
+        char arr[] = str.toCharArray();
+
+        Arrays.sort(arr);
+        for (int i = 0; i < len - 1; i++) {
+            if (arr[i] == arr[i + 1])
+                return false;
+        }
+        return true;
     }
 }
