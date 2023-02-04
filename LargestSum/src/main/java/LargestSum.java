4a7ee1c437b4a1e4f529818f2dcfcd9c37066954
Grammeri <reacttsxgrammer@gmail.com>
2023-02-04 06:26:46 +0000

    Saved progress on MostCommonCharacter 2023-02-04 06:26:46 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,7 +1,12 @@
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LargestSum {
+    int max1 = 0;
+    int max2 = 0;
     /**
      * Get the largest possible sum that can be obtained from a pair of values in
      * the list. A number can't be added
@@ -10,26 +15,20 @@ public class LargestSum {
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
+    int maximum;
     public int bigSum(List<Integer> nums) {
-       int endIndex = 0, currMax = nums.get(0);
-       int globalMax = nums.get(0);
+        nums = new ArrayList<Integer>();
+        max1 = Collections.max(nums);
+     nums.remove(nums.indexOf(max1));
+     max2 = Collections.max(nums);
 
-       for (int i=1; i< nums.size();++i){
-        currMax = Math.max(nums.get(i), nums.get(i) + currMax);
-        if(currMax > globalMax){
-            globalMax = currMax;
-            endIndex = i;
-        }
-       }
-       int startIndex = endIndex;
-       while (startIndex >=0){
-        globalMax -= nums.get(startIndex);
-
-        if (globalMax == 0)
-        break;
-        startIndex --;
-       }
-       
-    return globalMax;
+    return max1 + max2;
     }
 }
+
+/*nums = new ArrayList<Integer>();
+        Collections.sort(nums);
+        temp1=nums.get(nums.size()-1);
+        temp2=nums.get(nums.size()-2);
+
+        return temp1 + temp2;*/
