7c71a003b23c10b0985ff44210b5275f2027717c
Grammeri <reacttsxgrammer@gmail.com>
2023-02-04 07:01:07 +0000

    Saved progress on StringIsPalindrome 2023-02-04 07:01:07 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -1,17 +1,32 @@
 
 public class Palindrome {
     /**
-     * Palindromes are words that are the same forwards as they are backwards - eg, 'bob', 'racecar'.
+     * Palindromes are words that are the same forwards as they are backwards - eg,
+     * 'bob', 'racecar'.
      *
      * You can solve this problem in several ways:
-     * You could start with a String reversal algorithm, which you have already written, and check if str is the same
-     * as its reversed form, or, you could check if the character at the nth position of a String moving forwards
+     * You could start with a String reversal algorithm, which you have already
+     * written, and check if str is the same
+     * as its reversed form, or, you could check if the character at the nth
+     * position of a String moving forwards
      * re the same as the nth position of a String moving backwards.
      *
      * @param str A String.
      * @return true if str is a palindrome, false otherwise.
      */
-    public boolean pal(String str){
-        return false;
+    public boolean pal(String str) {
+
+        String rev = "";
+
+        boolean ans = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
     }
 }
