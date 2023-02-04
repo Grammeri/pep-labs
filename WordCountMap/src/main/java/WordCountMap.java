d6bc9e5f35b2fe9dac554a1894dd59cab195d4f6
Grammeri <reacttsxgrammer@gmail.com>
2023-02-04 07:24:29 +0000

    Saved progress on WordCountMap 2023-02-04 07:24:29 +0000

--- WordCountMap/src/main/java/WordCountMap.java
+++ WordCountMap/src/main/java/WordCountMap.java
@@ -1,21 +1,36 @@
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class WordCountMap {
     /**
      * Receive a string of multiple words.
-     * Return a Map object where the key is a word and the value is the count of occurrences of the word.
+     * Return a Map object where the key is a word and the value is the count of
+     * occurrences of the word.
      *
-     * You will need to instantiate a map, use the split(" ") on words, and iterate through the resulting array.
-     * Inside of the resulting for loop, you should insert a new key to the map every time you encounter a word that
-     * is not already a key in the map, and add to the word's count value every time you encounter a word that already
+     * You will need to instantiate a map, use the split(" ") on words, and iterate
+     * through the resulting array.
+     * Inside of the resulting for loop, you should insert a new key to the map
+     * every time you encounter a word that
+     * is not already a key in the map, and add to the word's count value every time
+     * you encounter a word that already
      * exists in the map.
      *
      * @param words A string of Words separated by spaces.
-     * @return a Map (key/value pairs) of all the words mapped to their number of occurrences.
+     * @return a Map (key/value pairs) of all the words mapped to their number of
+     *         occurrences.
      */
-    public Map<String, Integer> returnWordMap(String words){
-
-        return null;
+    public Map<String, Integer> returnWordMap(String words) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        String[] a = words.split(" ");
+        for (String word : a) {
+            Integer integer = hashMap.get(word);
+            if (integer == null)
+                hashMap.put(word, 1);
+            else {
+                hashMap.put(word, integer + 1);
+            }
+        }
+        return hashMap;
     }
 }
