a08b371d0f7169915a279c8adbb01b90d8b230e9
Grammeri <reacttsxgrammer@gmail.com>
2023-02-05 01:11:03 +0000

    Saved progress on LargestSum 2023-02-05 01:11:03 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -16,19 +16,22 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     int maximum;
-    public int bigSum(List<Integer> nums) {
-        nums = new ArrayList<Integer>();
-        max1 = Collections.max(nums);
-     nums.remove(nums.indexOf(max1));
-     max2 = Collections.max(nums);
 
-    return max1 + max2;
+    public int bigSum(List<Integer> nums) {
+        Collections.sort(nums, Collections.reverseOrder());
+        if (nums.get(0) == nums.get(1)) {
+            return 2 * nums.get(0);
+        } else {
+            return nums.get(0) + nums.get(1);
+        }
     }
 }
 
-/*nums = new ArrayList<Integer>();
-        Collections.sort(nums);
-        temp1=nums.get(nums.size()-1);
-        temp2=nums.get(nums.size()-2);
-
-        return temp1 + temp2;*/
+/*
+ * nums = new ArrayList<Integer>();
+ * Collections.sort(nums);
+ * temp1=nums.get(nums.size()-1);
+ * temp2=nums.get(nums.size()-2);
+ * 
+ * return temp1 + temp2;
+ */
